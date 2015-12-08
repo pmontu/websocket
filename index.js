99e5061a970c50b64cd2ab7c33235c459a462224
Manoj Kumar P <manoj.k@plancess.com>
2015-12-08 10:31:53 +0530

    testing socket with wanted environment

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
+var mongodb = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/challenger_documents');
+var ObjectId = mongodb.ObjectID;
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
@@ -11,18 +26,44 @@ server.listen(port)
 
 console.log("http server listening on %d", port)
 
+
+app.get('/rooms', function(req, res) {
+    console.log("Lising Rooms")
+    var room = db.get('room');
+    room.find({status:0},{},function(e,docs){
+        res.send(docs)
+    });
+});
+
+app.get('/last_room', function(req, res) {
+    console.log("Lising Rooms")
+    var room = db.get('room');
+    room.find({status:0},{},function(e,docs){
+        res.send(docs[docs.length-1])
+    });
+});
+
+app.get('/questions', function(req, res) {
+    var question = db.get('question');
+    question.find({},{},function(e,docs){
+        res.send(docs)
+    });
+});
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+console.log("server started")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+	ws.send("hi")
+	console.log("client connected")
 
-  console.log("websocket connection open")
+	ws.on("close", function() {
+		console.log("client disconnected")
+	})
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+	ws.on('message', function incoming(message) {
+	    console.log('received: %s', message);
+	    ws.send(message)
+	});
+
+
 })
