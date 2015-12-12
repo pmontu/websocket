af3b0816678553a5d79ebe0070a8582c120ac41a
Manoj Kumar P <manoj.k@plancess.com>
2015-12-12 10:36:02 +0530

    ported backend to websocket and tested

--- index.js
+++ index.js
@@ -137,6 +137,141 @@ wss.on("connection", function(ws) {
                         }
                     })
                     break;
+
+                case "ask question":
+                    // Question
+                    questionids = message.data
+                    console.log(id + " has asked " + questionids.length + " questions")
+                    var room = db.get("room")
+                    roomid = null
+
+                    room.find({owner:id, status:1},{},function(e,docs){
+                        if(docs.length != 0){
+                            console.log("owner")
+                            console.log(docs)
+                            roomid = docs[0]._id
+                            if(docs[0].player in clients){
+                                player = clients[docs[0].player]
+                                room.update({_id:docs[0]._id},{$set:{owner_question:questionids}},function(e,d){
+                                    console.log("updated owner questions: " + d)
+                                })
+                                if("player_question" in docs[0]){
+                                    console.log("game starts ",id," ",player.id)
+                                    // STATUS 2 FOR GAME STARTED
+                                    room.update({_id:roomid},{$set:{status:2}},function(e,d){
+                                        console.log("updated room status to 2(started): " + d)
+                                    })
+                                    send(player, "answer", questionids)
+                                    send(ws, "answer", docs[0].player_question)
+                                }
+                            } else {
+                                send(ws, "message", "Player has left the room")
+                            }
+                        }
+                    });
+                    room.find({player:id, status:1},{},function(e,docs){
+                        if(docs.length != 0){
+                            console.log("player")
+                            console.log(docs)
+                            roomid = docs[0]._id
+                            if(docs[0].owner in clients){
+                                owner = clients[docs[0].owner]
+                                room.update({_id:docs[0]._id},{$set:{player_question:questionids}},function(e,d){
+                                    console.log("updated player questions: " + d)
+                                })
+                                if("owner_question" in docs[0]){
+                                    console.log("game starts ",owner.id," ",id)
+                                    // STATUS 2 FOR GAME STARTED
+                                    room.update({_id:roomid},{$set:{status:2}},function(e,d){
+                                        console.log("updated room status to 2(started): " + d)
+                                    })
+                                    send(ws, "answer", docs[0].owner_question)
+                                    send(owner, "answer", questionids)
+                                }
+                            } else {
+                                send(ws, "message", "Owner has left the room")
+                            }
+                        }
+                    });
+                    break;
+
+                case "submit":
+                    // Evaluation
+                    answers = message.data
+                    ans_arr = {}
+                    ques_arr = []
+                    answers.forEach(function(answer) {
+                        ans_arr[answer.questionid] = answer.answer;
+                        ques_arr.push(answer.questionid)
+                    })
+
+                    var correct = {1:0,2:0,3:0}, wrong = {1:0,2:0,3:0}
+                    ques_arr = ques_arr.map(ObjectId);
+                    var question = db.get("question")
+                    question.find({_id:{$in:ques_arr}},{},function(e,questions){
+                        if(questions.length>0){
+                            questions.forEach(function(question){
+                                console.log(question._id," actual:",question.answer," provided:",ans_arr[question._id])
+                                if(question.answer == ans_arr[question._id]){
+                                    correct[question.level]++;
+                                } else {
+                                    wrong[question.level]++;
+                                }
+                            })
+                            console.log("correct: ",correct[1],correct[2],correct[3])
+                            console.log("wrong: ",wrong[1],wrong[2],wrong[3])
+                            var score = correct[1]+correct[2]*3+correct[3]*5
+                            console.log("score",score)
+                            var stuff = {"correct":correct,"wrong":wrong,"score":score}
+
+                            var room = db.get("room");
+                            room.find({owner:id,status:2},{},function(e,docs){
+                                console.log("owner", id," length: ",docs.length)
+                                if(docs.length!=0){
+                                    if(docs[0].player in clients){
+                                        player = clients[docs[0].player]
+                                        room.update({_id:docs[0]._id},{$set:{owner_answer:stuff}})
+                                        // RESULTS
+                                        if("player_answer" in docs[0]){
+                                            other_stuff = docs[0].player_answer
+                                            room.update({_id:roomid},{$set:{status:3}},function(e,d){
+                                                console.log("updated room status to 3(over): " + d)
+                                            })
+                                            console.log("game over owner",stuff,other_stuff)
+                                            send(ws, "close", {"yours":other_stuff,"mine":stuff})
+                                            send(player, "close", {"yours":stuff,"mine":other_stuff})
+                                        }
+                                    } else {
+                                        send(ws, "message", "Player has left the room")
+                                    }
+                                }
+                            })
+                            room.find({player:id,status:2},{},function(e,docs){
+                                console.log("player", id," length: ",docs.length)
+                                if(docs.length!=0){
+                                    if(docs[0].owner in clients){
+                                        owner = clients[docs[0].owner]
+                                        room.update({_id:docs[0]._id},{$set:{player_answer:stuff}})
+                                        // RESULTS
+                                        if("owner_answer" in docs[0]){
+                                            other_stuff = docs[0].owner_answer
+                                            room.update({_id:roomid},{$set:{status:3}},function(e,d){
+                                                console.log("updated room status to 3(over): " + d)
+                                            })
+                                            console.log("game over player",stuff,other_stuff)
+                                            send(owner, "close", {"yours":stuff,"mine":other_stuff})
+                                            send(ws, "close", {"yours":other_stuff,"mine":stuff})
+                                        }
+                                    } else {
+                                        send(ws, "message", "Player has left the room")
+                                    }
+                                }
+                            })
+                        }
+                    })
+
+                    
+                    break;
             }
         }
 	});
